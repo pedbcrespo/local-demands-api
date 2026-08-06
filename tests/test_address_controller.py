0a13be6f678ab75e8ae0f50c56e41b775fff9e23
Pedro <pedbcrespo@gmail.com>
2026-08-06 12:11:47 -0300

    implementando testes do address_controller

--- tests/test_address_controller.py
+++ tests/test_address_controller.py
@@ -2,6 +2,8 @@ from config.db_config import db
 from model.address import Address
 from model.enums import State
 
+BASE_URL = "/local-demands/address"
+
 def test_create_address_success(client):
     payload = {
         "street": "Rua das Flores",
@@ -10,12 +12,12 @@ def test_create_address_success(client):
         "state": "RJ"
     }
 
-    response = client.post("/local-demands/address/register", json=payload)
+    response = client.post(f"{BASE_URL}/register", json=payload)
     assert response.status_code == 200
     assert response.json["city"] == "Rio de Janeiro"
 
 def test_get_states(client):
-    response = client.get("/local-demands/address/state")
+    response = client.get(f"{BASE_URL}/state")
 
     assert response.status_code == 200
     assert len(response.json) == 27
@@ -45,7 +47,7 @@ def test_get_cities_by_state_success(client, app):
         db.session.add_all([add1, add2, add3])
         db.session.commit()
 
-    response = client.get(f"/local-demands/address/state/{State.RJ.code}")
+    response = client.get(f"{BASE_URL}/state/{State.RJ.code}")
     assert response.status_code == 200
     data = response.json
     
@@ -56,7 +58,25 @@ def test_get_cities_by_state_success(client, app):
     assert "São Paulo" not in cities
 
 def test_get_cities_by_state_empty_result(client):
-    response = client.get(f"/local-demands/address/state/{State.MG.code}")
+    response = client.get(f"{BASE_URL}/state/{State.MG.code}")
 
     assert response.status_code == 200
     assert response.json == []
+
+def test_delete_address(client, app):
+    with app.app_context():
+            add1 = Address(
+                street="Av. Atlântica",
+                district="Copacabana",
+                city="Rio de Janeiro",
+                state=State.RJ.code
+            )
+            add1.id = 1
+            db.session.add_all([add1])
+            db.session.commit()
+
+    address_id = 1
+    response = client.delete(f"{BASE_URL}/delete/{address_id}")
+
+    assert response.status_code == 200
+    assert response.json.get('error') is None
